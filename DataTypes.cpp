25235e657561ea0ec8376009f7febe6f1e54d36f
NarasimhaNaidu12102002 <136229811+NarasimhaNaidu12102002@users.noreply.github.com>
2023-06-12 14:14:45 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 9;
+    int c;
+    // a = 3;
+    // b = 4;
+    c = a + b;
+    // cout << c;
+    // cout << endl;
+    // cout << "a+b";
+    cout << a + b << endl
+         << "a+b";
+    return 0;
+}
