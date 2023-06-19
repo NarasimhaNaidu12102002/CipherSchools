bd23b58fd70ef9b27dd6f402cb07ae4fa842e075
NarasimhaNaidu12102002 <136229811+NarasimhaNaidu12102002@users.noreply.github.com>
2023-06-19 14:29:15 +0530

    Create Lecture9-i-CipherSchools.cpp

---
+++ Lecture9-i-CipherSchools.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int compare(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "Greater is " << compare(a, b) << endl;
+}
