3b1e942879d5ee4a92de1ab8d4f571b031ee557d
NarasimhaNaidu12102002 <136229811+NarasimhaNaidu12102002@users.noreply.github.com>
2023-06-21 14:26:40 +0530

    Create Lecture11-i-CipherSchools.cpp

---
+++ Lecture11-i-CipherSchools.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The size of int in c++ is " << sizeof(int) << endl;
+    cout << "The size of long long int in c++ is " << sizeof(long long int) << endl;
+    return 0;
+}
